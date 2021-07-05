98589eb2419c8a2480dad2257e6c9aa3f315b825
Kirk Thomas <dablumaroon@gmail.com>
2021-07-05 18:23:51 -0400

    Getting help from BCS.

--- index.js
+++ index.js
@@ -1,79 +1,64 @@
 const inquirer = require('inquirer');
+const { Engineer } = require('./lib/engineer');
+const { Intern } = require('./lib/intern');
+const { Manager } = require('./lib/manager');
+const teamBuilder = require('./src/TeamBuilder');
 
-const startQuestions = [
-    {
-        type: 'input',
-        message: 'Enter the team manager\'s name.',
-        name: 'mgrName',
-    },
-    {
-        type: 'input',
-        message: 'Enter the team manager\'s ID.',
-        name: 'mgrID',
-    },
-    {
-        type: 'input',
-        message: 'Enter the team manager\'s email.',
-        name: 'mgrEmail',
-    },
-    {
-        type: 'input',
-        message: 'Enter the team manager\'s office number.',
-        name: 'mgrOffice',
-    },
-    {
-        type: 'list',
-        message: 'Provide the name of an engineer or an intern.',
-        name: 'empMenu',
-        choices: ['Engineer', 'Intern', ''],
-    },
+var manager;
+var engineer;
+var intern;
+var selection;
 
-];
+function init() {
+    inquirer.prompt(teamBuilder.startQuestions).then((answers) => {
+        createManager(answers);
+        selection = answers.empMenu;
+        //while (selection != '') {
+            switch (answers.empMenu) {
+                case 'Engineer':
+                    inquirer.prompt(teamBuilder.engQuestions)
+                        .then((answers) => {
+                            createEngineer(answers);
+                        });
+                    break;
+                case 'Intern':
+                    inquirer.prompt(teamBuilder.intQuestions)
+                    .then((answers) => {
+                        createIntern(answers);
+                    });
+                    break;
+            }
+        //}
+    });
+};
 
-const engQuestions = [
-    {
-        type: 'input',
-        message: 'Enter the engineer\'s name.',
-        name: 'engName',
-    },
-    {
-        type: 'input',
-        message: 'Enter the engineer\'s ID.',
-        name: 'engID',
-    },
-    {
-        type: 'input',
-        message: 'Enter the engineer\'s email.',
-        name: 'engEmail',
-    },
-    {
-        type: 'input',
-        message: 'Enter the engineer\'s github user name.',
-        name: 'engGhUsername',
-    },
+init();
 
-];
+function createManager(data) {
+    manager = new Manager(data.mgrName, data.mgrEmail, data.mgrOffice);
+    console.log("Manager ID: " + manager.getId());
+    console.log(manager.getName());
+    console.log(manager.getEmail());
+    console.log(manager.getOfficeNumber());
+    console.log(manager.getRole());
 
-const intQuestions = [
-    {
-        type: 'input',
-        message: 'Enter the intern\'s name.',
-        name: 'intName',
-    },
-    {
-        type: 'input',
-        message: 'Enter the intern\'s ID.',
-        name: 'intID',
-    },
-    {
-        type: 'input',
-        message: 'Enter the intern\'s email.',
-        name: 'intEmail',
-    },
-    {
-        type: 'input',
-        message: 'Enter the intern\'s school.',
-        name: 'intSchool',
-    },
+}
+function createEngineer(data) {
+    engineer = new Engineer(data.engName, data.engEmail, data.engGhUsername);
 
-];
+    console.log("Engineer ID: " + engineer.getId());
+    console.log(engineer.getName());
+    console.log(engineer.getEmail());
+    console.log(engineer.getGithub());
+    console.log(engineer.getRole());
+}
+
+function createIntern(data) {
+    intern = new Intern(data.intName, data.intEmail, data.intSchool);
+
+    console.log("Intern ID: " + intern.getId());
+    console.log(intern.getName());
+    console.log(intern.getEmail());
+    console.log(intern.getSchool());
+    console.log(intern.getRole());
+}
---
+++ src/TeamBuilder.js
@@ -0,0 +1,78 @@
+const startQuestions = [
+    {
+        type: 'input',
+        message: 'Enter the team manager\'s name.',
+        name: 'mgrName',
+    },
+    {
+        type: 'input',
+        message: 'Enter the team manager\'s email.',
+        name: 'mgrEmail',
+    },
+    {
+        type: 'input',
+        message: 'Enter the team manager\'s office number.',
+        name: 'mgrOffice',
+    },
+    {
+        type: 'list',
+        message: 'Provide the name of an engineer or an intern.',
+        name: 'empMenu',
+        choices: ['Engineer', 'Intern', ''],
+    },
+
+];
+
+const engQuestions = [
+    {
+        type: 'input',
+        message: 'Enter the engineer\'s name.',
+        name: 'engName',
+    },
+    {
+        type: 'input',
+        message: 'Enter the engineer\'s email.',
+        name: 'engEmail',
+    },
+    {
+        type: 'input',
+        message: 'Enter the engineer\'s github user name.',
+        name: 'engGhUsername',
+    },
+    {
+        type: 'list',
+        message: 'Provide the name of an engineer or an intern.',
+        name: 'empMenu',
+        choices: ['Engineer', 'Intern', ''],
+    },
+];
+
+const intQuestions = [
+    {
+        type: 'input',
+        message: 'Enter the intern\'s name.',
+        name: 'intName',
+    },
+    {
+        type: 'input',
+        message: 'Enter the intern\'s email.',
+        name: 'intEmail',
+    },
+    {
+        type: 'input',
+        message: 'Enter the intern\'s school.',
+        name: 'intSchool',
+    },
+    {
+        type: 'list',
+        message: 'Provide the name of an engineer or an intern.',
+        name: 'empMenu',
+        choices: ['Engineer', 'Intern', ''],
+    },
+];
+
+module.exports = {
+    startQuestions,
+    engQuestions,
+    intQuestions
+}
